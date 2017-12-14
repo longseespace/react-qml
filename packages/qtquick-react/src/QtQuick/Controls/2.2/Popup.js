7e21ad66040c4296880652bfa59ac4ad7ea292a6
Tung Vu <me@tungv.com>
2017-12-14 10:35:45 +0700

    fix: missing comma

--- packages/qtquick-react/src/QtQuick/Controls/2.2/Popup.js
+++ packages/qtquick-react/src/QtQuick/Controls/2.2/Popup.js
@@ -2,7 +2,7 @@
   import { registerNativeComponentClass } from 'qml-renderer';
   import { createElement, Component } from 'react';
 
-  const qmlContent = "\nimport QtQuick.Controls 2.2\nPopup {\n  propery alias data: childItem.data\n  Item {\n    id: childItem\n  }\n}";
+  const qmlContent = "\nimport QtQuick.Controls 2.2\nPopup {\n  propery alias data: childItem.data;\n  Item {\n    id: childItem\n  }\n}";
 
   const NATIVE_COMPONENT_REGISTRY_NAME = 'QtQuick.Controls.Popup_2.2'
 
