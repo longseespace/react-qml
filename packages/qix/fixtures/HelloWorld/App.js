4b4d697e7dd5b04bd24eee3bd921d693bc3b268b
Long Nguyen <long@podzim.co>
2017-12-15 13:40:34 +0700

    refactor: simple App

--- packages/qix/fixtures/HelloWorld/App.js
+++ packages/qix/fixtures/HelloWorld/App.js
@@ -1,17 +1,36 @@
 import ApplicationWindow from 'qtquick-react/QtQuick/Controls/2.2/ApplicationWindow';
-import ColumnLayout from 'qtquick-react/QtQuick/Layouts/1.0/ColumnLayout'
+import GridLayout from 'qtquick-react/QtQuick/Layouts/1.0/GridLayout';
+import ColumnLayout from 'qtquick-react/QtQuick/Layouts/1.0/ColumnLayout';
 import Text from 'qtquick-react/QtQuick/2.7/Text';
+import Item from 'qtquick-react/QtQuick/2.7/Item';
 import * as React from 'react';
 
 class App extends React.Component {
   render() {
     return (
       <ApplicationWindow visible width={800} height={600} color="#F5FCFF">
-        <ColumnLayout>
-          <Text text="Welcome to Qix" font={{ pointSize: 20 }} />
-          <Text text="To get started, edit App.js" color="#333333"/>
-          <Text text="Press Cmd+R to reload,\nCmd+D for dev menu'" color="#333333" />
-        </ColumnLayout>
+        <GridLayout Layout={{ fillWidth: true, fillHeight: true }} columns={3} rows={3}>
+          <ColumnLayout Layout={{ row: 1, column: 1 }}>
+            <Text
+              text="Welcome to Qix"
+              font={{ pointSize: 20 }}
+              horizontalAlignment={Text.AlignJustify}
+              Layout={{ fillWidth: true }}
+            />
+            <Text
+              text="To get started, edit App.js"
+              color="#333333"
+              horizontalAlignment={Text.AlignJustify}
+              Layout={{ fillWidth: true }}
+            />
+            <Text
+              text="Press Cmd+R to reload,<br/>Cmd+D for dev menu"
+              color="#333333"
+              horizontalAlignment={Text.AlignJustify}
+              Layout={{ fillWidth: true }}
+            />
+          </ColumnLayout>
+        </GridLayout>
       </ApplicationWindow>
     );
   }
