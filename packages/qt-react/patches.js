438246ae537e4bb6395fdb0b08194caba2bfb244
Long Nguyen <long@podzim.co>
2017-12-26 14:02:00 +0700

    feat: add more patches for some Quick Control Components

--- packages/qt-react/patches.js
+++ packages/qt-react/patches.js
@@ -21,4 +21,129 @@ module.exports = {
     }
     `,
   },
+  'QtQuick.Controls.ApplicationWindow': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.Container': {
+    defaultProp: 'contentData',
+  },
+  'QtQuick.Controls.Dialog': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.DialogButtonBox': {
+    defaultProp: 'contentData',
+  },
+  'QtQuick.Controls.Drawer': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.Frame': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.GroupBox': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.Menu': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.MenuBar': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.Page': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.ScrollView': {
+    defaultProp: 'contentData',
+  },
+  'QtQuick.Controls.SwipeView': {
+    defaultProp: 'contentData',
+  },
+  'QtQuick.Controls.TabBar': {
+    defaultProp: 'contentData',
+  },
+  'QtQuick.Controls.ToolBar': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
+  'QtQuick.Controls.ToolTip': {
+    defaultProp: 'contentData',
+    customDeps: ['QtQuick.Layouts'],
+    customQml: `
+    property alias children: content.data;
+
+    contentItem: ColumnLayout {
+      id: content;
+    }
+    `,
+  },
 };
