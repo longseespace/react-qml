35332479c10c1d6a609a8e5f223bc0685da8baa8
Long Nguyen <long@podzim.co>
2019-01-02 15:17:31 +0700

    refactor: fix getBabelConfig test

--- packages/cli/src/utils/getBabelConfig.js
+++ packages/cli/src/utils/getBabelConfig.js
@@ -57,16 +57,16 @@ module.exports = function getBabelConfig(cwd: string) {
     babelrc = DEFAULT_BABELRC;
   }
 
-  return Object.assign({}, babelrc, {
-    plugins: [require.resolve('./fixRequireIssues')]
-      .concat(
-        process.env.NODE_ENV === 'production'
-          ? []
-          : [
-              require.resolve('react-hot-loader/babel'),
-              require.resolve('../hot/babelPlugin'),
-            ]
-      )
-      .concat(babelrc.plugins || []),
-  });
+  const defaultPlugins = [require.resolve('./fixRequireIssues')]
+    .concat(
+      process.env.NODE_ENV === 'production'
+        ? []
+        : [
+            require.resolve('react-hot-loader/babel'),
+            require.resolve('../hot/babelPlugin'),
+          ]
+    )
+    .concat(babelrc.plugins || []);
+
+  return Object.assign({}, babelrc, { plugins: defaultPlugins });
 };
