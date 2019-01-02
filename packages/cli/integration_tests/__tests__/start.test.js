b57d552800a1dfaa1b3231c45422999171a5b434
Long Nguyen <long@podzim.co>
2019-01-02 16:28:55 +0700

    refactor: start tests

--- packages/cli/integration_tests/__tests__/start.test.js
+++ packages/cli/integration_tests/__tests__/start.test.js
@@ -5,6 +5,8 @@
  * @flow
  */
 
+// TODO: better integration test
+
 const { runHaul, runHaulSync } = require('../runHaul');
 const { cleanup, writeFiles } = require('../utils');
 const path = require('path');
@@ -15,7 +17,7 @@ const stripAnsi = require('strip-ansi');
 const TEMP_DIR = path.resolve(os.tmpdir(), 'start_test');
 const TEST_PROJECT_DIR = path.resolve(
   __dirname,
-  '../../fixtures/react-native-with-haul'
+  '../../../example-hello-world'
 );
 
 beforeAll(() => run('yarn --mutex network', TEST_PROJECT_DIR));
@@ -24,11 +26,15 @@ afterEach(() => cleanup(TEMP_DIR));
 
 test('start command displays "Select platform" message', () => {
   writeFiles(TEMP_DIR, {
-    'webpack.haul.js': '{}',
+    'react-qml.config.js': '{}',
   });
 
   const { stdout } = runHaulSync(TEMP_DIR, ['start']);
   expect(stripAnsi(stdout).trim()).toMatchSnapshot();
+});
+
+test('starts server and bundling macOS platform', done => {
+  testPlatform('macos', done);
 });
 
 test('starts server and bundling iOS platform', done => {
@@ -40,7 +46,7 @@ test('starts server and bundling Android platform', done => {
 });
 
 test('starts server and bundling all platforms', done => {
-  testPlatform('android', done);
+  testPlatform('all', done);
 });
 
 function testPlatform(platform, done) {
@@ -60,10 +66,15 @@ function testPlatform(platform, done) {
       haul.kill();
     }
 
-    if (message.match('bundling your React Native app')) {
+    if (message.match('bundling your React QML app')) {
       const stdout = messageBuffer.join('\n');
       expect(stdout).toMatch('INFO  Ready at http://localhost:8081');
       done();
+      haul.kill();
+    }
+
+    if (message.match('is already in use. What should we do?')) {
+      done(new Error('Port is in use'));
       haul.kill();
     }
   });
