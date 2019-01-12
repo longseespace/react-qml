376c8f735dabf67c513357468892b0d8730449e9
Long Nguyen <long@podzim.co>
2019-01-12 09:26:46 +0700

    chore: remove `example-rust`

--- packages/example-rust/App.js
+++
@@ -1,6 +0,0 @@
-import createQmlComponent from './createQmlComponent';
-import qmlSource from './App.qml';
-
-const App = createQmlComponent(qmlSource, 'App');
-
-export default App;
--- packages/example-rust/rust/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=framework=/usr/local/opt/qt5/Frameworks");
-    println!("cargo:rustc-link-lib=c++");
-}
