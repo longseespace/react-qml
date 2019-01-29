12454aaab0de220f275f624d9d2065bfb02c9add
Long Nguyen <long@podzim.co>
2019-01-29 18:42:31 +0700

    chore: add qmltypes definitions

---
+++ packages/react-qml/qmltypes/generateAllModules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+node ./generateModule.js qtlabscalendar.qmltypes QtLabsCalendar
+node ./generateModule.js qtlabsplatform.qmltypes QtLabsPlatform
+node ./generateModule.js qtlabssettings.qmltypes QtLabsSettings
+node ./generateModule.js qtquick.qmltypes QtQuick
+node ./generateModule.js qtquicklayouts.qmltypes QtQuickLayouts
+node ./generateModule.js qtquickwindow.qmltypes QtQuickWindow
